8f24539c635562c3a0c3ff980c35691c69acb55b
enginbukum <yasengin2005@gmail.com>
2023-08-12 10:02:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './Home'
+import Sidebar from './Sidebar'
+import Modal from './Modal'
+
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Sidebar />
+      <Modal />
+    </>
+  )
+}
+
+export default App
---
+++ src/Sidebar.jsx
@@ -0,0 +1,43 @@
+import { FaTimes } from "react-icons/fa";
+import logo from "./logo.svg";
+import { links, social } from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" className="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, icon, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
